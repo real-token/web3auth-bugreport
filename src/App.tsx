0986947c91d5c5a546dbdac714ea9c66d5f88875
chichke <bastien.silhol@epitech.eu>
2024-10-17 13:41:49 -0500

    feat: added patch package

--- src/App.tsx
+++ src/App.tsx
@@ -117,7 +117,7 @@ function App() {
       try {
         const web3authProvider = await web3auth.connectTo<AuthLoginParams>(
           WALLET_ADAPTERS.AUTH,
-          { loginProvider: "google" }
+          { loginProvider: "line" }
         );
         setProvider(web3authProvider);
       }
